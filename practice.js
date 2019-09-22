e0cea769a0201e7115085e2018b4de623f90ea1d
salvador becerra <salbecerra1023@gmail.com>
2019-09-22 15:18:35 -0700

    new problem

--- practice.js
+++ practice.js
@@ -19,3 +19,25 @@ var topKFrequent = function(nums, k) {
     let sorted = Object.keys(histogram).sort((a,b) => histogram[b] - histogram[a])
     return sorted.slice(0,k)
 };
+
+var distributeCandies = function(candies, num_people) {
+    let startArr = new Array(num_people).fill(0)
+    let candy = 1;
+    let index = 0;
+    let numOfCandies = candies
+    while (numOfCandies > 0) {
+        if (index == num_people) {
+            index = 0
+        }
+        if (candy <= numOfCandies) {
+            startArr[index] += candy
+            numOfCandies -= candy
+        } else {
+            startArr[index] += numOfCandies
+            numOfCandies =0
+        }
+        candy += 1
+        index++
+    }
+    return startArr
+};
