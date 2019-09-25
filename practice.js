1b166bc256e6a35af563b0b1caf703c1e737fd2a
salvador becerra <salbecerra1023@gmail.com>
2019-09-25 15:21:20 -0700

    new problem

--- practice.js
+++ practice.js
@@ -77,3 +77,13 @@ var numComponents = function(head, G) {
     }
     return count
 };
+
+var middleNode = function(head) {
+    var fast = head;
+    var slow = head;
+    while(fast !== null && fast.next !== null) {
+        fast = fast.next.next
+        slow = slow.next
+    }
+    return slow
+};
